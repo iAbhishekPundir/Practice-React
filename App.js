f21448d8e5f9235b14cff5036ca1efc8000fc16a
Abhishek <pundirabhishek987@gmail.com>
2023-12-18 08:04:47 +0530

    refactor code

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-const div = 
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = 
 React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"}, 
         [React.createElement("h1",{}, "This is h1 heading"),
-        React.createElement("h2",{}, "This is h2 heading")]),
+        React.createElement("h2",{}, "This is h2 heading save it")]),
         React.createElement("div",{id:"child2"}, 
         [React.createElement("h1",{}, "This is h1 heading"),
         React.createElement("h2",{}, "This is h2 heading")])]
@@ -15,4 +17,4 @@ React.createElement("div",{id:"parent"},
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(div);
+        root.render(parent);
