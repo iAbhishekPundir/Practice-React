e7c4bf3d4d9133a1f381454d1d0af8537a07e72b
Abhishek <pundirabhishek987@gmail.com>
2023-12-19 08:39:56 +0530

    add: Component composing

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ const heading = <h1 id="heading">Namaste React using JSX🚀</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const Title = () => <h2 className="title">This is my title for this page</h2>;
+
 const HeadingComponent = () => {
-    return(
-        <h1>This is my first functional component</h1>
-    )
-}
+  return (
+    <div id="container">
+      <Title />
+      <h1>This is my first functional component</h1>
+    </div>
+  );
+};
 root.render(heading);
-root.render(<HeadingComponent/>);
+root.render(<HeadingComponent />);
