4924a106ce122e97c7c7bf54df61a433080c652c
Abhishek <pundirabhishek987@gmail.com>
2023-12-20 06:49:29 +0530

    add: different ways of rendering a functional component

--- App.js
+++ App.js
@@ -6,13 +6,17 @@ const heading = <h1 id="heading">Namaste React using JSX🚀</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const number = 1999;
-const Title = () => <h2 className="title">This is my title for this page</h2>;
+const Title = () => <div>
+    <h2 className="title">This is my title for this page</h2>
+</div>;
 
 const HeadingComponent = () => {
   return (
     <div id="container">
         {number}
       <Title />
+      <Title></Title>
+      {Title()}
       <h1>This is my first functional component</h1>
     </div>
   );
