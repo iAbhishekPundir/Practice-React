57a3fdf55688a6f620bef612cae1024db0858732
Abhishek <pundirabhishek987@gmail.com>
2023-12-21 07:59:40 +0530

    add: Header & Restaurant Card component

--- App.js
+++ App.js
@@ -1,25 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1 id="heading">Namaste React using JSX🚀</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const number = 1999;
-const Title = () => <div>
-    <h2 className="title">This is my title for this page</h2>
-</div>;
-
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-        {number}
-      <Title />
-      <Title></Title>
-      {Title()}
-      <h1>This is my first functional component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://tse4.mm.bing.net/th?id=OIP.RdNN9GEvTFDohpXmSeeplwHaGd&pid=Api&P=0&h=220"
+        />
+      </div>
+      <div className="nav-container">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
-root.render(heading);
-root.render(<HeadingComponent />);
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card-container">
+      <div className="res-image">
+        <img src="https://tse3.explicit.bing.net/th?id=OIP.pg1I4TPfxsWIuC8G4pOdsAHaF_&pid=Api&P=0&h=220" />
+      </div>
+      <div className="res-info">
+        <p>Name: Apna Dhaba</p>
+        <p>Rating: ⭐⭐⭐⭐</p>
+        <p>Cusines: Burger, Salad, Fries, Pasta, Badam-ragda</p>
+      </div>
+    </div>
+  );
+};
+
+const Seacrh = () => {
+  return <div className="search">Search</div>;
+};
+const Body = () => {
+  return (
+    <div>
+      <Seacrh />
+      <div className="res-card-wrapper">
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
