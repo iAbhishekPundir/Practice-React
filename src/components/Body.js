fda478b4c2a1ec7d9685b506a7f124b4e58f126d
Abhishek <pundirabhishek987@gmail.com>
2023-12-25 15:16:17 +0530

    refactor: code

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,11 @@ import { useState } from "react";
 import RestaurantCard from "../components/RestaurantCard";
 import { restaurantList } from "../utils/mockData";
 
-
-
-// const Search = () => {
-//   return (
-//     <div className="search">
-//       <button onClick={() => setData(data.filter((res) => res.avgRating >= 4))}>
-//         Top Rated Restaurants
-//       </button>
-//     </div>
-//   );
-// };
-
 const Body = () => {
     const [data, setData] = useState(restaurantList);
     console.log(data);
   return (
     <div>
-      {/* <Search /> */}
       <div className="filter">
       <button onClick={()=>{setData(restaurantList?.filter((res)=> res.data.avgRating >= 4))}} >
         Top Rated Restaurants
