102760bca5e43f03f16f747d66c119655cb1c333
Abhishek <pundirabhishek987@gmail.com>
2023-12-25 13:11:20 +0530

    refactor: code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,37 @@
-import RestaurantCard from "../components/RestaurantCard"
+import { useState } from "react";
+import RestaurantCard from "../components/RestaurantCard";
 import { restaurantList } from "../utils/mockData";
 
-const Search = () => {
-    return <div className="search">Search</div>;
-  };
 
+
+// const Search = () => {
+//   return (
+//     <div className="search">
+//       <button onClick={() => setData(data.filter((res) => res.avgRating >= 4))}>
+//         Top Rated Restaurants
+//       </button>
+//     </div>
+//   );
+// };
 
 const Body = () => {
-    return (
-      <div>
-        <Search />
-        <div className="res-card-wrapper">
-          {
-            restaurantList.map((restaurant) =>   <RestaurantCard key={restaurant.data.id} resData={restaurant} />)
-          }
-        
-        </div>
+    const [data, setData] = useState(restaurantList);
+    console.log(data);
+  return (
+    <div>
+      {/* <Search /> */}
+      <div className="filter">
+      <button onClick={()=>{setData(restaurantList?.filter((res)=> res.data.avgRating >= 4))}} >
+        Top Rated Restaurants
+      </button>
+    </div>
+      <div className="res-card-wrapper">
+        {data?.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Body;
