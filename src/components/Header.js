a116a732ca549df46cd26b6d3841baa3de65b1b9
Abhishek <pundirabhishek987@gmail.com>
2023-12-26 14:18:43 +0530

    feat: conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-container">
         <ul className="nav-items">
@@ -15,6 +14,15 @@ const Header = () => {
           <li>About</li>
           <li>Contact us</li>
           <li>Cart</li>
+          {isLogin ? (
+            <button className="login-btn" onClick={() => setIsLogin(false)}>
+              Logout
+            </button>
+          ) : (
+            <button className="login-btn" onClick={() => setIsLogin(true)}>
+              Login
+            </button>
+          )}
         </ul>
       </div>
     </div>
