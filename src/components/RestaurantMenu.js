e9a30177171bc83d8a6b47020139b6fb88bcd97c
Abhishek <pundirabhishek987@gmail.com>
2023-12-30 08:02:45 +0530

    fix : bug

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resMenu = useRestaurantMenu(resId);
 
-  if (resMenu === null) {
+  const [showIndex, setShowIndex] = useState(1);
+
+
+  if (resMenu === null || resMenu === undefined) {
     return <Shimmer />;
   }
 
@@ -22,28 +27,46 @@ const RestaurantMenu = () => {
     locality,
     name,
     sla,
-  } = resMenu;
+    feeDetails,
+    totalRatingsString,
+  } = resMenu?.data?.cards[2]?.card?.card?.info;
 
   const { deliveryTime, lastMileTravelString } = sla;
 
+  const categories =
+    resMenu?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+  console.log("categories", categories);
+
+  const { message } = feeDetails;
+
   return (
-    <div className="m-4 p-4 bg-slate-200 w-60" key={id}>
-      <h1 className="font-bold">Name of the Restaurant</h1>
-      <p className="font-semibold"> {name} </p>
-      <p className="font-semibold"> {areaName} </p>
-      <p className="font-semibold"> {avgRatingString}⭐ </p>
-      <p className="font-semibold"> {city} </p>
-      <h1 className="font-bold"> Menu </h1>
-      <ul>
-        <p> {cuisines.join(", ")} </p>
-      </ul>
-      <p className="font-semibold"> {locality} </p>
-      <p>
-        <b>
-          {" "}
-          {deliveryTime}min -- {lastMileTravelString}{" "}
-        </b>
-      </p>
+    <div className="w-screen bg-slate-50">
+      <div
+        className="p-4 w-6/12 bg-slate-300 mx-auto my-4 shadow-lg rounded-sm flex justify-between"
+        key={id}
+      >
+        <div>
+          <h1 className="font-bold text-lg p-1"> {name} </h1>
+          <p className="text-slate-700 p-1">{cuisines.join(", ")}</p>
+          <p className="text-slate-700 p-1">
+            {areaName}, {lastMileTravelString}🔻
+          </p>
+          <p>🚲 {message}</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-green-600 ">⭐ {avgRatingString} </p>
+          <p className="text-gray-400 "> {totalRatingsString} </p>
+        </div>
+      </div>
+      <div className="p-4 w-6/12  mx-auto my-4 ">
+        {categories.map((category, index) => (
+          <RestaurantCategory 
+          resCategoryData={category?.card?.card}
+          showItems= {index === showIndex ? true : false}
+          setShowIndex={()=> setShowIndex(index)}
+           />
+        ))}
+      </div>
     </div>
   );
 };
