225bdb5191db9951ab04ce0f456104c8aca401d0
Abhishek <pundirabhishek987@gmail.com>
2023-12-30 08:03:48 +0530

    feat : RestaurantCategory and ItemList Component added

---
+++ src/components/ItemList.js
@@ -0,0 +1,26 @@
+import { CDN_URL } from "../utils/constants";
+
+const ItemList = ({ itemListData }) => {
+  console.log("itemList", itemListData);
+  return (
+    <div>
+      {itemListData !== undefined &&
+        itemListData.map((item) => (
+          <div key={item?.card?.info?.id} className="flex justify-between items-center my-4 p-2 shadow-lg bg-slate-100">
+            <div className="m-2 p-2">
+              <h2 className="m-2 font-bold">{item?.card?.info?.name}</h2>
+              <p className="m-2 font-semibold"> ₹{item?.card?.info?.price / 100} <span className="text-red-600">| 10% swiggy off </span></p>
+              <p className="m-2 text-gray-600"> {item?.card?.info?.description} </p>
+            </div>
+            
+            <div className="w-36  ">
+                <button className="absolute bg-white text-green-500 font-semibold hover:bg-slate-200 rounded-br-xl hover:text-green-600">Add +</button>
+                <img className="rounded-md" src={CDN_URL + item?.card?.info?.imageId}  />
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ItemList;
---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+
+const RestaurantCategory = ({ resCategoryData, showItems, setShowIndex }) => {
+
+    const handleClick = () => {
+        setShowIndex();
+        
+    }
+  return (
+    <div>
+      {resCategoryData?.itemCards?.length !== undefined && (
+        <div className="">
+          <div onClick={handleClick} className="bg-slate-200 flex justify-between m-2 px-4 py-2 rounded-sm shadow-lg hover:cursor-pointer">
+            <span className="text-large font-bold">
+              {resCategoryData?.title} ({resCategoryData?.itemCards?.length})
+            </span>
+            <span >{resCategoryData?.title && showItems ? "⬆️" : "⬇️" }</span>
+          </div>
+
+          <div className="">
+            { showItems && <ItemList itemListData={resCategoryData?.itemCards} />}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RestaurantCategory;
