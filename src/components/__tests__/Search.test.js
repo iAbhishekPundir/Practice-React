3ab1242b77bacd161df565bb1811c05a8d5ed4b7
Abhishek <pundirabhishek987@gmail.com>
2024-01-04 21:15:26 +0530

    feat : Test case for Search restaurants

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,67 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/RestaurantListMockData.json";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+
+describe("Test cases for Search functionality", () => {
+  it("should test filtering Restaurant with Search ", async () => {
+    await act(async () =>
+      render(
+        <BrowserRouter>
+          <Body />
+        </BrowserRouter>
+      )
+    );
+
+    const cardBeforeSearch = screen.getAllByTestId("resCard");
+
+    expect(cardBeforeSearch.length).toBe(9);
+
+    const searchBtn = screen.getByRole("button", { name: "Search" });
+    expect(searchBtn).toBeInTheDocument();
+
+    const searchInput = screen.getByTestId("searchInput");
+
+    fireEvent.change(searchInput, { target: { value: "pizza" } });
+
+    fireEvent.click(searchBtn);
+
+    //screen should load 1 card
+
+    const cardAfterSearch = screen.getAllByTestId("resCard");
+
+    expect(cardAfterSearch.length).toBe(1);
+  });
+
+  it("should show top rated restaurants", async () => {
+    await act(async () =>
+      render(
+        <BrowserRouter>
+          <Body />
+        </BrowserRouter>
+      )
+    );
+    const topRatedBtn = screen.getByRole("button", {
+      name: "Top Rated Restaurants",
+    });
+    expect(topRatedBtn).toBeInTheDocument();
+
+    const resCard = screen.getAllByTestId("resCard");
+    expect(resCard.length).toBe(9);
+
+    fireEvent.click(topRatedBtn);
+
+    const topRatedResCard = screen.getAllByTestId("resCard");
+    expect(topRatedResCard.length).toBe(4);
+  });
+});
