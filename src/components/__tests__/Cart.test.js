724fcf75a36e7a93d7c7a31a226c4c3ef58c2bda
Abhishek <pundirabhishek987@gmail.com>
2024-01-07 21:24:51 +0530

    add : test cases for Cart Component

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -1,9 +1,12 @@
 import RestaurantMenu from "../RestaurantMenu";
+import Cart from "../Cart";
+import Header from "../Header";
 import MOCK_DATA from "../mocks/RestaurantMenuMockData.json";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
 import { appStore } from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
@@ -14,18 +17,36 @@ global.fetch = jest.fn(() =>
 it("should load Restaurant Menu component", async () => {
   await act(async () =>
     render(
+      <BrowserRouter>
       <Provider store={appStore}>
+        <Header />
         <RestaurantMenu />
+        <Cart />
       </Provider>
+      </BrowserRouter>
     )
   );
+ 
+  const recommendedMenuBtn = screen.getByText("Recommended (20)");
+  // console.log(recommendedMenuBtn);
+  fireEvent.click(recommendedMenuBtn);
 
-  // const recommendedMenuBtn = screen.getByText("Recommended (20)");
-  //   console.log(recommendedMenuBtn);
-  // fireEvent.click(recommendedMenuBtn);
+  expect(screen.getAllByTestId("foodItems").length).toBe(20);
 
-  // const menuItem = screen.getByAltText("Primavera Gourmet-Pizza");
-
-  // const addBtn = screen.getByRole("button", { name: "Add +" });
+  const addBtn = screen.getAllByRole("button", { name: "Add +" });
   // console.log(addBtn);
+  expect(screen.getByText("Cart (0)")).toBeInTheDocument();
+  
+  fireEvent.click(addBtn[0]);
+
+  expect(screen.getByText("Cart (1)")).toBeInTheDocument();
+
+
+  const cartBtn = screen.getByText("Cart (1)");
+  fireEvent.click(cartBtn);
+
+  const cartItem = screen.getAllByText("Primavera Gourmet-Pizza");
+
+  expect(cartItem).toBeInTheDocument();
+
 });
