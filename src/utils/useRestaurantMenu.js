7ce97285ab61f115dffad00efed170956684997a
Abhishek <pundirabhishek987@gmail.com>
2024-09-26 22:16:21 +0530

    data refactoring

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MENU_URL } from "./constants";
+import RESTAURANT_MENU_MOCK_DATA from "../mocks/RestaurantMenuMockData.json";
 
 const useRestaurantMenu = (resId) => {
   const [resMenu, setResMenu] = useState(null);
@@ -7,8 +8,9 @@ const useRestaurantMenu = (resId) => {
   const fetchData = async (resId) => {
     const fetchedData = await fetch(MENU_URL + resId);
     const jsonData = await fetchedData.json();
-    // console.log("Restaurant Menu : ",jsonData);
+    console.log("Restaurant Menu : ",jsonData);
     setResMenu(jsonData);
+    // setResMenu(RESTAURANT_MENU_MOCK_DATA);
   };
 
   useEffect(() => {
